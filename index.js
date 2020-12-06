8f0b8178e23d7d3310bf774e9944a63f1cdbece6
Itai G <click.itai@gmail.com>
2020-12-06 10:46:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { register, listen } = require('push-receiver');
 const senderId = "632057595650"; // This is the `project_number` from the google-services.json (It is possible to extract it from apk!)
 
-if (!senderId) {
-  console.error('Missing senderId');
-  return;
-}
+if (!senderId) { console.error('Missing senderId'); return; }
 
 (async () => {
   
-  // Register to FCM and get Token
+  // Register to FCM and get new Token
   const credentials = await register(senderId); // You should call register only once and then store the credentials somewhere
+  
   const fcmToken = credentials.fcm.token; // <-- Token to use to send notifications
   console.log('Your Token: ', fcmToken);
   
@@ -19,7 +17,7 @@ if (!senderId) {
   
 })();
 
-// called on new notification
+// This function called on new notification
 function onNotification({ notification }) {
   // Do someting with the notification
   console.log('Notification received!!');
